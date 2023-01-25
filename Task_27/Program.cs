7c1b3388f543eba2b71b5445fb9a08047f4b8b3e
William-J-Butcher <William.J.Billy.Butcher@yandex.ru>
2023-01-25 23:48:49 +0300

    Добавил задачу № 27

---
+++ Task_27/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 27: 
+//Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int sum = 0;
+int temp = num;
+for (int i = 0; i < num; i++)
+{
+    int remainder = temp % 10;
+    sum += remainder;
+    temp /=  10;
+}
+Console.WriteLine($"Сумма цифр в числе = {sum}");
