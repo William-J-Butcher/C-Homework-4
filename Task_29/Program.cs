45d3ede97c5bb3bbcc4b70f92e7edffa7db188c5
William-J-Butcher <William.J.Billy.Butcher@yandex.ru>
2023-01-26 00:03:11 +0300

    Добавил задачу № 29

---
+++ Task_29/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 29: 
+// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+Console.WriteLine("Программа выводит массив из N элементов случайных чисел");
+Console.Write("Введите любое целое число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
+int[] array = new int[new Random().Next(0, num)];
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(0, num);
+    Console.WriteLine(array[i]);
+}
+Console.WriteLine();
