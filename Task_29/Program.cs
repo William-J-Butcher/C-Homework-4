97828f185f16e582bf22e9d310f938919393bbf6
William-J-Butcher <William.J.Billy.Butcher@yandex.ru>
2023-01-26 00:27:53 +0300

    Доработал задачу № 29

--- Task_29/Program.cs
+++ Task_29/Program.cs
@@ -3,6 +3,7 @@
 // 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
 // 6, 1, 33 -> [6, 1, 33]
 
+Console.WriteLine();
 Console.WriteLine("Программа выводит массив из N элементов случайных чисел");
 Console.Write("Введите любое целое число: ");
 int num = Convert.ToInt32(Console.ReadLine());
@@ -11,6 +12,7 @@ int[] array = new int[new Random().Next(0, num)];
 for (int i = 0; i < array.Length; i++)
 {
     array[i] = new Random().Next(0, num);
-    Console.WriteLine(array[i]);
+    // Console.WriteLine(array[i]);
 }
+Console.WriteLine("[{0}]", string.Join(", ", array));
 Console.WriteLine();
